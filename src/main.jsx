ed76f42554e2314b476b5a5bda34ca69507c6349
Matheus Link <linksauermths2203@gmail.com>
2023-12-13 10:21:56 -0300

    Corrigindo uma rota e tbm os cards para telas menores

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { path: "/useContextLearn", element: <Home /> },
-      { path: "/useContextLearn/contact", element: <Contact /> },
+      { path: "/contact", element: <Contact /> },
     ],
   },
 ]);
